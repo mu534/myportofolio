8dfeabe81e16b1c44bd65da6d381a8fcb19ffcfc
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-02-21 12:53:34 +0000

    updated project changes

--- src/Page/Project.tsx
+++ src/Page/Project.tsx
@@ -1,51 +1,36 @@
-// components/Project.tsx
 import { Github } from "lucide-react";
 import React from "react";
 
 interface ProjectProps {
   title: string;
-  // description: string;
-
   imageUrl: string;
 }
 
-const Project: React.FC<ProjectProps> = ({
-  title,
-  // description,
-
-  imageUrl,
-}) => {
+const Project: React.FC<ProjectProps> = ({ title, imageUrl }) => {
   return (
-    <>
-      <div
-        id="Project"
-        className="flex items-start border p-2 m-6 border-purple-600"
-      >
+    <div className="border border-purple-600 pl-3 pr-3 m-6 ml-6 rounded-lg max-w-lg mx-auto">
+      <h2 className="text-purple-500 font-inter text-3xl font-bold text-center mb-3">
+        {title}
+      </h2>
+      <div className="p-0 ">
         <img
           alt={title}
           src={imageUrl}
-          className="w-32 h-32 object-cover mr-2" // Adjust the size of the image and margin between image and text
+          className="w-full h-34 object-contain rounded-md"
         />
-        <div>
-          <h2 className=" text-purple-500 font-inter text-3xl font-bold">
-            {title}
-          </h2>
-          {/* <p className="text-white font-inter text-lg  font-light">
-            {description}
-          </p> */}
-          <div className="flex space-x-4 items-center justify-center">
-            <a
-              href="https://github.com/mu534"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="object-cover mr-7"
-            >
-              <Github className="w-8 h-8 mt-4 text-gray-400 hover:text-white" />
-            </a>
-          </div>
-        </div>
       </div>
-    </>
+      <div className="flex justify-center mt-4 pb-2">
+        <a
+          href="https://github.com/mu534"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="flex items-center justify-center p-2 bg-purple-700 rounded-lg hover:bg-purple-700 transition duration-300">
+            <Github className="w-8 h-8 text-white" />
+          </button>
+        </a>
+      </div>
+    </div>
   );
 };
 
